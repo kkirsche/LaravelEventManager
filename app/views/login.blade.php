9ce2a076b0310f92493cdbdad41fac10879ca846
Kevin Kirsche <kev.kirsche@gmail.com>
2013-06-14 17:23:33 -0400

    Update with icons

--- app/views/login.blade.php
+++ app/views/login.blade.php
@@ -7,12 +7,12 @@
             <h2>Login</h2>
             <form action="{{ URL::to('/login') }}" method="POST">
                 <fieldset>
-                    <label>Username:</label>
+                    <label><i class="icon-user"></i> Username:</label>
                     <input type="text" name="username" class="span6" placeholder="Username" />
-                    <label>Password:</label>
+                    <label><i class="icon-key"></i> Password:</label>
                     <input type="password" name="password" class="span6" placeholder="Password">
                     <p><a href="{{ URL::to('/registerUser') }}" title="Create An Account">Create An Account</a></p>
-                    <button class="btn btn-primary" type="submit">Sign in</button>
+                    <button class="btn btn-primary" type="submit">Sign in <i class="icon-signin"></i></button>
                 </fieldset>
             </form>
             </div>
