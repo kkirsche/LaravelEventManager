10862604f6b7516e24ee33f19e58742d78f14e8e
Kevin Kirsche <kev.kirsche@gmail.com>
2013-06-15 10:26:29 -0400

    Add error catch

--- app/views/login.blade.php
+++ app/views/login.blade.php
@@ -5,6 +5,13 @@
         <div class="row">
             <div class="span6 offset3">
             <h2>Login</h2>
+            @if (Session::has('error'))
+                <div class="alert alert-error">
+                      <button type="button" class="close" data-dismiss="alert">&times;</button>
+                      <strong>Oops!</strong> {{ trans(Session::get('reason')) }}
+                </div>
+            {{ Session::flush() }}
+            @endif
             <form action="{{ URL::to('/login') }}" method="POST">
                 <fieldset>
                     <label><i class="icon-user"></i> Username:</label>
