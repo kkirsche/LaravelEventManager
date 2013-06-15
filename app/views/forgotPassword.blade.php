d9a1c97b18ef37ec947e8c6d26e4fe60cc4da16a
Kevin Kirsche <kev.kirsche@gmail.com>
2013-06-15 10:26:18 -0400

    Add error/success handling

--- app/views/forgotPassword.blade.php
+++ app/views/forgotPassword.blade.php
@@ -5,6 +5,18 @@
         <div class="row">
             <div class="span6 offset3">
             <h2>Forgot Password</h2>
+            @if (Session::has('error'))
+                <div class="alert alert-error">
+                      <button type="button" class="close" data-dismiss="alert">&times;</button>
+                      <strong>Oops!</strong> {{ trans(Session::get('reason')) }}
+                </div>
+                {{ Session::flush() }}
+            @elseif (Session::has('success'))
+                <div class="alert alert-success">
+                      <button type="button" class="close" data-dismiss="alert">&times;</button>
+                      <strong>Yay!</strong> An e-mail with the password reset has been sent.
+                </div>
+            @endif
             <form action="{{ URL::to('/forgotPassword') }}" method="POST">
                 <fieldset>
                     {{ Form::label('email', 'Email:') }}
