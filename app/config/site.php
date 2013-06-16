505a7e7417307aa4265f68abdef3b3bcfad6dcec
Kevin Kirsche <kev.kirsche@gmail.com>
2013-06-16 12:53:13 -0400

    Fix comment

--- app/config/site.php
+++ app/config/site.php
@@ -7,9 +7,7 @@ return array(
 	| Site Title
 	|--------------------------------------------------------------------------
 	|
-	| When your application is in debug mode, detailed error messages with
-	| stack traces will be shown on every error that occurs within your
-	| application. If disabled, a simple generic error page is shown.
+	| This is the site title displayed as the <title> and in the mneu bar.
 	|
 	*/
 
