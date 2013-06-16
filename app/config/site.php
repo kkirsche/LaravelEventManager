7dcd7b180ba434c4b10bd1cc38dcb67de4bb385f
Kevin Kirsche <kev.kirsche@gmail.com>
2013-06-16 12:52:34 -0400

    Add site config for title

---
+++ app/config/site.php
@@ -0,0 +1,28 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Site Title
+	|--------------------------------------------------------------------------
+	|
+	| When your application is in debug mode, detailed error messages with
+	| stack traces will be shown on every error that occurs within your
+	| application. If disabled, a simple generic error page is shown.
+	|
+	*/
+
+	'title' => 'H-SC Event Manager',
+
+	/*
+	|--------------------------------------------------------------------------
+	| Open Registrations
+	|--------------------------------------------------------------------------
+	|
+	| This is to show whether or not any user can sign up to be a user on  your site
+	|
+	*/
+
+	'openRegistrations' => true,
+);
