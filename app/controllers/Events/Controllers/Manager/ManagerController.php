0c4f71cfbe03f701c8cdd2482a8b185ecc5f68db
Kevin Kirsche <kev.kirsche@gmail.com>
2013-06-17 20:01:30 -0400

    Update the controller to include comments and createEvent method

--- app/controllers/Events/Controllers/Manager/ManagerController.php
+++ app/controllers/Events/Controllers/Manager/ManagerController.php
@@ -2,15 +2,17 @@
 
 namespace Events\Controllers\Manager;
 
-use \View, \BaseController, \Site, \Input;
+use \View, \BaseController, \Site, \CalendarEvent, \Input, \Validator;
 
 class ManagerController extends BaseController
 {
+    //This control ensures the user is logged in before they can view any of the manager pages or use the manager actions
 	public function __construct()
 	{
 		$this->beforeFilter('auth');
 	}
 
+    //This control gets the correct manager page and displays it
 	public function getManager($nav = null)
 	{
 		if ($nav == null)
@@ -23,6 +25,7 @@ class ManagerController extends BaseController
         }
 	}
 
+    //This control takes the site settings page when it is POSTed and moves it to the model to update the site settings.
     public function postSiteSettings()
     {
         $siteSettings = array(
@@ -35,5 +38,59 @@ class ManagerController extends BaseController
         Site::postSiteSettings($siteSettings);
 
         return View::make('manager/settings');
+    }
+
+    public function postCreateEvent()
+    {
+        $eventDetails = array(
+            'eventCalendar'         => Input::get('eventCalendar'),
+            'eventName'             => Input::get('eventName'),
+            'eventDescription'      => Input::get('eventDescription'),
+            'eventCapacity'         => Input::get('eventCapacity'),
+            'eventRegistrationType' => Input::get('eventRegistrationType'),
+            'eventLocationName'     => Input::get('eventLocationName'),
+            'eventLocationAddress1' => Input::get('eventLocationAddress1'),
+            'eventLocationAddress2' => Input::get('eventLocationAddress2'),
+            'eventLocationAddress3' => Input::get('eventLocationAddress3'),
+            'eventLocationCity'     => Input::get('eventLocationCity'),
+            'eventLocationState'    => Input::get('eventLocationState'),
+            'eventLocationZIP'      => Input::get('eventLocationZIP'),
+            'eventLocationCountry'  => Input::get('eventLocationCountry'),
+            'eventLocationPhone'    => Input::get('eventLocationPhone'),
+            'eventStartDate'        => Input::get('eventStartDate'),
+            'eventEndDate'          => Input::get('eventEndDate'),
+            'eventStartTime'        => Input::get('eventStartTime'),
+            'eventEndTime'          => Input::get('eventEndTime'),
+            'eventRSVPEndDate'      => Input::get('eventRSVPEndDate'),
+            'eventPublishDate'      => Input::get('eventPublishDate'),
+            'eventArchiveDate'      => Input::get('eventArchiveDate')
+        );
+
+        $rules = array(
+            'eventName' => 'required',
+            'eventDescription' => 'required',
+            'eventCapacity' => 'integer',
+            'eventLocationZIP' => 'integer',
+            'eventStartDate' => 'date'
+        );
+
+        $v = Validator::make($eventDetails, $rules);
+
+        if ($v->passes())
+        {
+            $event = new CalendarEvent;
+            if ($event->postCreateEvent($eventDetails))
+            {
+                return View::make('manager/createEvent');
+            }
+            else
+            {
+                echo "WTF....";
+            }
+        }
+        else
+        {
+            return View::make('manager/');
+        }
     }
 }
